9b902175fdecac65be336f42cf93b582ac55aeab
MattMcDonald <mail.matt.mcdonald@gmail.com>
2014-09-01 16:27:40 -0700

    using a real index page

--- src/galah/handler.clj
+++ src/galah/handler.clj
@@ -1,10 +1,11 @@
 (ns galah.handler
+  (:use ring.util.response)
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
