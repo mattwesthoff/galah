000003d3335f446c5c8bdc25b7b7dd68cb4b8f7b
MattMcDonald <mail.matt.mcdonald@gmail.com>
2014-09-01 20:58:09 -0700

    really simple test of the chat-handler

--- test/galah/test/handler.clj
+++ test/galah/test/handler.clj
@@ -17,34 +17,15 @@
                    :websocket true
 		   })
      ~@body))
-
-; (defmacro with-rejecting-handler [handler & body]
-;   `(with-server (start-http-server ~handler
-; 		  {:port 8007
-;                    :websocket true
-;                    :websocket-handshake-handler (fn [ch# _#] (enqueue ch# {:status 404}))
-; 		   })
-;      ~@body))
-
-(defn echo-handler [ch req]
-  (siphon ch ch))
-
+     
 (defn ws-client []
   (websocket-client {:url "ws://localhost:8007"}))
 
-(deftest test-echo-handler
-  (with-handler echo-handler
+(deftest test-chat-handler
+  (with-handler chat-handler
     (let [ch (wait-for-result (ws-client) 1000)]
+      (enqueue ch "b")
       (dotimes [_ 100]
         (enqueue ch "a")
-        (is (= "a" (wait-for-result (read-channel ch) 500))))
+        (is (= "b: a" (wait-for-result (read-channel ch) 500))))
       (close ch))))
-
-; (deftest test-handshake-handler
-;   (with-rejecting-handler echo-handler
-;     (is (thrown? Exception (wait-for-result (ws-client) 1000)))))
-;
-
-(deftest test-app
-  (testing "main route"
-    (is true)))
